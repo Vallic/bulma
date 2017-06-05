6110064e77be0cb6f3fb95ce2e0e5c165285f873
Valentino Medimorec <valic@valic.me>
2017-06-06 00:30:37 +0200

    JS adjustments for starterkit

--- starterkit/default/assets/js/default.js
+++ starterkit/default/assets/js/default.js
@@ -1,13 +1,20 @@
 /**
  * @file
- * Custom scripts for theme.
+ * BULMA_SUBTHEME_MACHINE_NAME scripts for theme.
  */
 (function ($) {
   'use strict';
 
-  // Hello World.
-  Drupal.behaviors.default = {
+  Drupal.behaviors.BULMA_SUBTHEME_MACHINE_NAME = {
     attach: function (context) {
+
+      var $toggle = $('#nav-toggle');
+      var $menu = $('#nav-menu');
+
+      $toggle.click(function () {
+        $(this).toggleClass('is-active');
+        $menu.toggleClass('is-active');
+      });
 
     }
   };

e338ee0a678055c8286863ee4621d43c3264aec0
nedjo <nedjo@4481.no-reply.drupal.org>
2017-08-21 14:38:14 -0700

    Issue #2901810 by nedjo: No effect when burger menu clicked

--- assets/js/bulma.js
+++ assets/js/bulma.js
@@ -7,15 +7,15 @@
 
   Drupal.behaviors.bulma = {
     attach: function (context) {
+      var $toggle = $('#nav-toggle').once('bulma');
+      if ($toggle.length) {
+        var $menu = $('#nav-menu');
 
-      var $toggle = $('#nav-toggle');
-      var $menu = $('#nav-menu');
-
-      $toggle.click(function () {
-        $(this).toggleClass('is-active');
-        $menu.toggleClass('is-active');
-      });
-
+        $toggle.click(function () {
+          $(this).toggleClass('is-active');
+          $menu.toggleClass('is-active');
+        });
+      }
     }
   };
 
